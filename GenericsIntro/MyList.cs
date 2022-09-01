8b3b5c8445f583e5d1b9f26f6ddc4d0696dcbda3
egecitak <egecitak01@gmail.com>
2022-09-01 14:26:39 +0300

    Kodlama.io C#/Angular Bootcamp Day 4 - End of Lesson.

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    internal class MyList<T>
+    {
+        T[] items;
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add (T item)
+        {
+            T[] tempArray = items; //to save the previous values at the old reference 
+            items = new T[items.Length + 1]; //create a new array with length + 1
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            
+            items[items.Length - 1] = item;
+        }
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace GenericsIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating a list without collections
+            MyList<string> names = new MyList<string>();
+
+            names.Add("Engin");
+        }
+    }
+}
